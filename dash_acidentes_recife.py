b7dc94c92165ed74ea07a7901ceeaa69d5da9756
Rodolpho Vasconcelos <rodolphovictor25@gmail.com>
2024-01-19 15:33:34 -0300

    Desenvolvimento dashboard

--- dash_acidentes_recife.py
+++ dash_acidentes_recife.py
@@ -3,8 +3,10 @@ import pandas as pd
 import plotly.express as px
 import numpy as np
 import plotly.graph_objects as go
+import json
 
 st.set_page_config(page_title="Dashboard Acidentes",layout="wide")
+st.image("data/logo1.png",caption="")
 st.title("Acidentes Recife 2022 - Prefeitura Municipal de Recife")
 
 with open('style.css')as f:
@@ -16,6 +18,18 @@ st.sidebar.image("data/logo1.png",caption="")
 st.sidebar.header("Filtros")
 
 df = pd.read_csv("acidentes2022.csv", sep=';')
+
+categorias = list(df['natureza'].unique())
+categorias.append('TODAS')
+categoria = st.sidebar.selectbox('Selecione a natureza do acidente', options = categorias,index=None, placeholder="Selecione uma opção:")
+
+if categoria == 'COM VÍTIMA':
+    df = df[df['natureza'] == 'COM VÍTIMA']
+elif categoria == 'SEM VÍTIMA':
+    df = df[df['natureza'] == 'SEM VÍTIMA']
+elif categoria == 'VÍTIMA FATAL':
+    df = df[df['natureza'] == 'VÍTIMA FATAL']
+
 df = df.drop(columns=['hora', 'numero','detalhe_endereco_acidente','complemento', 'num_semaforo', 'sentido_via','velocidade_max_via', 'divisao_via1', 'divisao_via2', 'divisao_via3' ])
 
 colunas_drop = ['vitimasfatais', 'auto', 'moto', 'ciclom', 'ciclista', 'pedestre', 'onibus', 'caminhao', 'viatura', 'outros', 'vitimas']
@@ -32,6 +46,9 @@ soma_onibus = df['onibus'].sum()
 soma_caminhao = df['caminhao'].sum()
 soma_viatura = df['viatura'].sum()
 soma_outros = df['outros'].sum()
+
+if categoria == 'COM VÍTIMA':
+    df = df[df['natureza'] == 'COM VÍTIMA']
 
 st.subheader('Em :blue[2022], na cidade do Recife, a quantidade total de veículos envolvidos em acidentes se distribui da seguinte forma:')
 
@@ -63,6 +80,7 @@ with col8:
 
 st.markdown(f"<h4>Além dos <span style='font-size:1.5em;'>{soma_pedestre}</span> pedestres que também se envolveram em acidentes em 2022 no Recife.</h4>", unsafe_allow_html=True)
 
+#Tratamento de dados para o gráfico de Linha
 df['data'] = pd.to_datetime(df['data'])
 df['mes'] = df['data'].dt.month_name()
 nomes_mes = {"January": "Janeiro","February": "Fevereiro","March": "Março","April": "Abril","May": "Maio","June": "Junho","July": "Julho","August": "Agosto","September": "Setembro","October": "Outubro","November": "Novembro","December": "Dezembro"}
@@ -72,11 +90,87 @@ df['mes'] = df['mes'].astype(meses_ordem)
 df = df.sort_values(by='mes')
 acidentes_por_mes = df['mes'].value_counts().sort_index()
 media_acidentes_por_mes = acidentes_por_mes.rolling(window=2).mean()
-fig = px.line(x=acidentes_por_mes.index, y=acidentes_por_mes.values, labels={'x': '', 'y': ''}, title='Evolução Mensal dos Acidentes de Trânsito em 2022', color_discrete_sequence=px.colors.sequential.Blues_r)
-fig.update_layout(xaxis=dict(showgrid=False), yaxis=dict(showgrid=False,showticklabels=False))
-fig.add_trace(go.Scatter(x=acidentes_por_mes.index, y=media_acidentes_por_mes.values,
-                         mode='lines', name='Média móvel(2 meses)', line=dict(color='red', dash='dash')))
-st.plotly_chart(fig, use_container_width=True, theme="streamlit")  
+
+#Tratamento de dados para o gráfico Treemap
+index_null_tempo = list(df[df['tempo_clima'].isnull()].index)
+
+for index in index_null_tempo:
+    date_tempo = df.loc[index, 'data']
+    aux_df = df[df['data'] == date_tempo]
+    value_mode = aux_df['tempo_clima'].mode()
+    
+    if len(value_mode) > 0:
+        df.loc[index, 'tempo_clima'] = value_mode.iloc[0]
+
+# Preencher valores nulos restantes com "Não especificado"
+df['tempo_clima'].fillna("Não especificado", inplace=True)
+
+# Agrupar por 'tempo_clima' e somar 'vitimas'
+acidentes_por_clima = df.groupby('tempo_clima')['vitimas'].sum().reset_index()
+
+
+#Tratamento de dados para o gráfico de barra Dia útil/FDS
+df['dia_da_semana'] = df['data'].dt.day_name()
+def classificar_dia(dia):
+    if dia in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']:
+        return 'Dia Útil'
+    else:
+        return 'Final de Semana'
+df['tipo_dia'] = df['dia_da_semana'].apply(classificar_dia)
+quantidade_acidentes_por_tipo_dia = df['tipo_dia'].value_counts()
+
+#Tratamento de dados para gráfico de mapa
+local_path = 'data/bvisualizacao_fcbairro.geojson'
+with open(local_path, 'r', encoding='utf-8') as file:
+    state_geo = json.load(file)
+acidentes_por_bairro = df.groupby('bairro')['vitimas'].count()
+acidentes_por_bairro = acidentes_por_bairro.reset_index()
+
+#Tratamento de dados para gráfico barra - tipo de acidentes
+if categoria == 'VÍTIMA FATAL':
+    tipo_acidente = df.groupby('tipo')['vitimasfatais'].sum()
+else:
+    tipo_acidente = df.groupby('tipo')['vitimas'].sum()
+    tipo_acidente = tipo_acidente[tipo_acidente > 5]
+tipo_acidente = tipo_acidente.sort_values(ascending=True)
+
+#tratamento de dados para gráfico quantidade de vitimas
+vitimas = df.groupby('natureza')['vitimas'].sum()
+vitimas = vitimas.sort_values(ascending=False)
+
+col9, col10 = st.columns(2, gap="small")
+with col9:
+    fig_clima = px.treemap(acidentes_por_clima, path=['tempo_clima'], values="vitimas", color_discrete_sequence=px.colors.sequential.Blues_r, title='Considerando o clima no momento do acidente temos a seguinte distribuição:')
+    st.plotly_chart(fig_clima, use_container_width=True, theme="streamlit") 
+    
+
+with col10:
+    fig_dia = px.bar(x=quantidade_acidentes_por_tipo_dia.index, y=quantidade_acidentes_por_tipo_dia.values, labels={'x': '', 'y': ''}, title='Quantidade de Acidentes por Tipo de Dia')
+    fig_dia.update_layout(xaxis=dict(showgrid=False), yaxis=dict(showgrid=False,showticklabels=False))
+    st.plotly_chart(fig_dia, use_container_width=True, theme="streamlit") 
+
+with st.container():
+    fig = px.line(x=acidentes_por_mes.index, y=acidentes_por_mes.values, labels={'x': '', 'y': ''}, title='Evolução Mensal dos Acidentes de Trânsito em 2022', color_discrete_sequence=px.colors.sequential.Blues_r)
+    fig.update_layout(xaxis=dict(showgrid=False), yaxis=dict(showgrid=False,showticklabels=False))
+    fig.add_trace(go.Scatter(x=acidentes_por_mes.index, y=media_acidentes_por_mes.values, mode='lines', name='Média móvel(2 meses)', line=dict(color='red', dash='dash')))
+    st.plotly_chart(fig, use_container_width=True, theme="streamlit")  
+
+col11, col12 = st.columns(2, gap="small")
+with col11:
+    fig_tipo = px.bar(tipo_acidente, x=tipo_acidente, labels={'x': '', 'y': ''},title='Vítimas por Tipo de Acidente', color_discrete_sequence=px.colors.sequential.Blues_r)
+    fig_tipo.update_layout(xaxis=dict(showgrid=False), yaxis=dict(showgrid=False))
+    st.plotly_chart(fig_tipo, use_container_width=True, theme="streamlit") 
+    
+
+with col12:
+    fig_vitimas = px.pie(vitimas, values='vitimas', names=vitimas.index, color_discrete_sequence=px.colors.sequential.Blues_r, title='Vítimas')
+    fig_vitimas.update_layout(xaxis=dict(showgrid=False), yaxis=dict(showgrid=False,showticklabels=False))
+    st.plotly_chart(fig_vitimas, use_container_width=True, theme="streamlit") 
+
+fig_mapa = px.choropleth(acidentes_por_bairro, geojson=state_geo, color="vitimas", locations="bairro", featureidkey="properties.EBAIRRNOME", projection="mercator")
+fig_mapa.update_geos(fitbounds="geojson", visible=True)
+fig_mapa.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+st.plotly_chart(fig_mapa, use_container_width=True, theme="streamlit") 
 
 
 
