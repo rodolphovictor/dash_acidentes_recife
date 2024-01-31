e6bdb628f6f3bcc85cbf8732eaf681914e4fa9ec
rodolphovasconcelos <rodolphovictor25@gmail.com>
2024-01-30 22:08:49 -0300

    Add ano(2021)

--- dash_acidentes_recife.py
+++ dash_acidentes_recife.py
@@ -6,8 +6,8 @@ import plotly.graph_objects as go
 import json
 
 st.set_page_config(page_title="Dashboard Acidentes",layout="wide")
-st.image("data/logo1.png",caption="")
-st.title("Acidentes Recife 2022 - Prefeitura Municipal de Recife")
+#st.image("data/logo1.png",caption="")
+st.title("Acidentes Recife - Prefeitura Municipal de Recife")
 
 with open('style.css')as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
@@ -17,10 +17,28 @@ st.sidebar.image("data/logo1.png",caption="")
 
 st.sidebar.header("Filtros")
 
-df = pd.read_csv("acidentes2022.csv", sep=';')
+@st.cache_data
+def load_data(file_path):
+    return pd.read_csv(file_path, sep=';')
+
+df_2021 = load_data("data/acidentes2021.csv")
+df_2022 = load_data("data/acidentes2022.csv")
+
+df_2021.rename(columns={'natureza_acidente':'natureza'}, inplace = True)
+
+
+colunas_conv = ['vitimasfatais', 'auto', 'moto', 'ciclom', 'ciclista', 'pedestre', 'onibus', 'caminhao', 'viatura', 'outros', 'vitimas']
+for coluna in colunas_conv:
+    df_2022[coluna] = df_2022[coluna].str.split(',').str[0].astype(int)
+    df_2021[coluna] = df_2021[coluna].fillna(0)
+
+#df = pd.read_csv("data/acidentes2022.csv", sep=';')
+df = pd.concat([df_2021, df_2022])
 
 df['data'] = pd.to_datetime(df['data'])
 df['mes'] = df['data'].dt.month_name()
+df['ano'] = df['data'].dt.year
+#st.dataframe(df)
 nomes_mes = {"January": "Janeiro","February": "Fevereiro","March": "Março","April": "Abril","May": "Maio","June": "Junho","July": "Julho","August": "Agosto","September": "Setembro","October": "Outubro","November": "Novembro","December": "Dezembro"}
 df['mes'] = df['mes'].apply(lambda mes: nomes_mes[mes])
 meses_ordem = pd.CategoricalDtype(categories=['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'], ordered=True)
@@ -39,15 +57,22 @@ if categoria == 'TODAS':
 else:
     df = df[df['natureza'] == categoria]
 
+anos = list(df['ano'].unique())
+anos.append('TODOS ANOS')
+default_index_1 = anos.index('TODOS ANOS')
+ano = st.sidebar.selectbox('Selecione o ano', options = anos,index=default_index_1, placeholder="Selecione uma opção:")
+
+
+if ano == 'TODOS ANOS':
+    df = df
+else:
+    df = df[df['ano'] == ano]
+
 selected_range = st.sidebar.select_slider('Selecione um intervalo de meses', options=range(1, 13),value=(1, 12),format_func=lambda x: meses_dict[x])
 
 df = df[(df['mes_num'] >= selected_range[0]) & (df['mes_num'] <= selected_range[1])]
 
 df = df.drop(columns=['hora', 'numero','detalhe_endereco_acidente','complemento', 'num_semaforo', 'sentido_via','velocidade_max_via', 'divisao_via1', 'divisao_via2', 'divisao_via3' ])
-
-colunas_drop = ['vitimasfatais', 'auto', 'moto', 'ciclom', 'ciclista', 'pedestre', 'onibus', 'caminhao', 'viatura', 'outros', 'vitimas']
-for coluna in colunas_drop:
-    df[coluna] = df[coluna].str.split(',').str[0].astype(int)
 
 soma_vitimasfatais = df['vitimasfatais'].sum()
 soma_auto = df['auto'].sum()
@@ -63,7 +88,7 @@ soma_outros = df['outros'].sum()
 if categoria == 'COM VÍTIMA':
     df = df[df['natureza'] == 'COM VÍTIMA']
 
-st.subheader('Em :blue[2022], na cidade do Recife, a quantidade total de veículos envolvidos em acidentes se distribui da seguinte forma:')
+st.subheader(f'Em :blue[{ano}], na cidade do Recife, a quantidade total de veículos envolvidos em acidentes se distribui da seguinte forma:')
 
 col1, col2, col3, col4 = st.columns(4, gap="small")
 with col1:
@@ -96,15 +121,15 @@ st.markdown(f"<h4>Além dos <span style='font-size:1.5em;'>{soma_pedestre}</span
 #Tratamento de dados para o gráfico de Linha
 
 df = df.sort_values(by='mes')
-acidentes_por_mes = df['mes'].value_counts().sort_index()
-media_acidentes_por_mes = acidentes_por_mes.rolling(window=2).mean()
+acidentes_por_mes = df.groupby(['mes', 'ano']).size().reset_index(name='quantidade')
+acidentes_por_mes['media_movel'] = acidentes_por_mes['quantidade'].rolling(window=2).mean()
 
 #Tratamento de dados para o gráfico Treemap
 index_null_tempo = list(df[df['tempo_clima'].isnull()].index)
 
 for index in index_null_tempo:
     date_tempo = df.loc[index, 'data']
-    aux_df = df[df['data'] == date_tempo]
+    aux_df = df[df['data'].astype(str) == str(date_tempo)].reset_index(drop=True)
     value_mode = aux_df['tempo_clima'].mode()
     
     if len(value_mode) > 0:
@@ -114,7 +139,10 @@ for index in index_null_tempo:
 df['tempo_clima'].fillna("Não especificado", inplace=True)
 
 # Agrupar por 'tempo_clima' e somar 'vitimas'
-acidentes_por_clima = df.groupby('tempo_clima')['vitimas'].sum().reset_index()
+if categoria == 'VÍTIMA FATAL':
+    acidentes_por_clima = df.groupby('tempo_clima')['vitimasfatais'].sum().reset_index()
+else:
+    acidentes_por_clima = df.groupby('tempo_clima')['vitimas'].sum().reset_index()
 
 
 #Tratamento de dados para o gráfico de barra Dia útil/FDS
@@ -131,7 +159,10 @@ quantidade_acidentes_por_tipo_dia = df['tipo_dia'].value_counts()
 local_path = 'data/bairros.geojson'
 with open(local_path, 'r', encoding='utf-8') as file:
     state_geo = json.load(file)
-acidentes_por_bairro = df.groupby('bairro')['vitimas'].count()
+if categoria == 'VÍTIMA FATAL':
+    acidentes_por_bairro = df.groupby('bairro')['vitimasfatais'].count()
+else:
+    acidentes_por_bairro = df.groupby('bairro')['vitimas'].count()
 acidentes_por_bairro = acidentes_por_bairro.reset_index()
 
 #Tratamento de dados para gráfico barra - tipo de acidentes
@@ -139,7 +170,7 @@ if categoria == 'VÍTIMA FATAL':
     tipo_acidente = df.groupby('tipo')['vitimasfatais'].sum()
 else:
     tipo_acidente = df.groupby('tipo')['vitimas'].sum()
-    tipo_acidente = tipo_acidente[tipo_acidente > 5]
+    tipo_acidente = tipo_acidente[tipo_acidente > 10]
 tipo_acidente = tipo_acidente.sort_values(ascending=True)
 
 #tratamento de dados para gráfico quantidade de vitimas
@@ -148,7 +179,11 @@ vitimas = vitimas.sort_values(ascending=False)
 
 col9, col10 = st.columns(2, gap="small")
 with col9:
-    fig_clima = px.treemap(acidentes_por_clima, path=['tempo_clima'], values="vitimas", color_discrete_sequence=px.colors.sequential.Blues_r, title='Considerando o clima no momento do acidente temos a seguinte distribuição:')
+    if categoria == 'VÍTIMA FATAL':
+        fig_clima = px.treemap(acidentes_por_clima, path=['tempo_clima'], values="vitimasfatais", color_discrete_sequence=px.colors.sequential.Blues_r, title='Considerando o clima no momento do acidente temos a seguinte distribuição:')
+    else:
+        fig_clima = px.treemap(acidentes_por_clima, path=['tempo_clima'], values="vitimas", color_discrete_sequence=px.colors.sequential.Blues_r, title='Considerando o clima no momento do acidente temos a seguinte distribuição:')
+
     st.plotly_chart(fig_clima, use_container_width=True, theme="streamlit") 
     
 
@@ -158,9 +193,9 @@ with col10:
     st.plotly_chart(fig_dia, use_container_width=True, theme="streamlit") 
 
 with st.container():
-    fig = px.line(x=acidentes_por_mes.index, y=acidentes_por_mes.values, labels={'x': '', 'y': ''}, title='Evolução Mensal dos Acidentes de Trânsito em 2022', color_discrete_sequence=px.colors.sequential.Blues_r)
+    fig = px.line(acidentes_por_mes, x='mes', y='quantidade', color='ano', labels={'x': '', 'y': ''}, title='Evolução Mensal dos Acidentes de Trânsito em 2022', color_discrete_sequence=px.colors.sequential.Blues_r)
     fig.update_layout(xaxis=dict(showgrid=False), yaxis=dict(showgrid=False,showticklabels=False))
-    fig.add_trace(go.Scatter(x=acidentes_por_mes.index, y=media_acidentes_por_mes.values, mode='lines', name='Média móvel(2 meses)', line=dict(color='red', dash='dash')))
+    fig.add_trace(go.Scatter(x=acidentes_por_mes['mes'], y=acidentes_por_mes['media_movel'], mode='lines', name='Média móvel(2 meses)', line=dict(color='red', dash='dash')))
     st.plotly_chart(fig, use_container_width=True, theme="streamlit")  
 
 col11, col12 = st.columns(2, gap="small")
@@ -171,10 +206,19 @@ with col11:
     
 
 with col12:
-    # fig_vitimas = px.pie(vitimas, values='vitimas', names=vitimas.index, color_discrete_sequence=px.colors.sequential.Blues_r, title='Vítimas')
-    # fig_vitimas.update_layout(xaxis=dict(showgrid=False), yaxis=dict(showgrid=False,showticklabels=False))
-    # st.plotly_chart(fig_vitimas, use_container_width=True, theme="streamlit") 
-    fig_mapa = px.choropleth(acidentes_por_bairro, geojson=state_geo, color="vitimas", locations="bairro", featureidkey="properties.EBAIRRNOME", projection="mercator",title='Acidentes por Bairro')
+    if categoria == 'VÍTIMA FATAL':
+        fig_mapa = px.choropleth(acidentes_por_bairro, geojson=state_geo, color="vitimasfatais", locations="bairro", featureidkey="properties.EBAIRRNOME", projection="mercator")
+    else:
+        fig_mapa = px.choropleth(acidentes_por_bairro, geojson=state_geo, color="vitimas", locations="bairro", featureidkey="properties.EBAIRRNOME", projection="mercator")
     fig_mapa.update_geos(fitbounds="geojson", visible=False)
-    fig_mapa.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig_mapa.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, title_text = 'Acidentes por Bairro')
     st.plotly_chart(fig_mapa, use_container_width=True, theme="streamlit") 
+
+# with st.container():
+#     nomes_dias = {"Monday": "Segunda", "Tuesday": "Terça", "Wednesday": "Quarta", "Thursday": "Quinta", "Friday": "Sexta", "Saturday": "Sábado", "Sunday": "Domingo"}
+#     df['dia_da_semana'] = df['dia_da_semana'].map(nomes_dias)
+#     ordenacao = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+#     dados_agrupados = df.groupby(['ano', 'dia_da_semana']).size().reset_index(name='quantidade')
+#     fig_dia_semana = px.bar(dados_agrupados, x='dia_da_semana', y='quantidade', color='ano', title="Acidentes por dia da semana", color_discrete_sequence=px.colors.sequential.Blues_r, category_orders={'dia_da_semana': ordenacao})
+#     fig_dia_semana.update_layout(xaxis=dict(title='', showgrid=False),yaxis=dict(title='', showgrid=False), barmode = 'group')
+#     st.plotly_chart(fig_dia_semana, use_container_width=True, theme="streamlit")
